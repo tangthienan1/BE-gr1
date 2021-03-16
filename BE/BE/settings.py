b03b338bcbd27b2637635f0b8cf8b65623c2cb1c
quoc4897 <leanhquoc11145@gmail.com>
2021-03-16 23:16:03 +0700

    [BE][quoclagcs18222] config database

--- BE/BE/settings.py
+++ BE/BE/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'BE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb',
-        'USER': 'postgres',
-        'PASSWORD': '822596',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
