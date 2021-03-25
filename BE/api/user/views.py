c0dbd77f9109046f15340ff7fbb7ce80b80af16e
Huy Khuong <scottkhuong@gmail.com>
2021-03-26 01:48:07 +0700

    [BE][huykcgcs18249] Finished Faculty, User, Role and JWT

--- BE/api/user/views.py
+++ BE/api/user/views.py
@@ -1,3 +1,8 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import AllowAny
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,14 +11,28 @@ from django.http import Http404
 from rest_framework import mixins
 from rest_framework import generics
 from .models import User
-from .serializers import UserSerializer
+from .serializers import CustomUserSerializer
 
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = CustomUserSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = CustomUserSerializer
+
+class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
