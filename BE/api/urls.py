d9e8b4164aa54b92d633a188251a265fcd265d6a
An Tang <tangthienan9@gmail.com>
2021-03-30 22:05:01 +0700

    [TL][anttgcs18437] fix media folder

--- BE/api/urls.py
+++ BE/api/urls.py
@@ -6,10 +6,10 @@ from .views import home
 
 urlpatterns = [
     path('', home, name='api.home'),
-    # path('comment/', include('api.comment.urls')),
-    # path('contribution/', include('api.contribution.urls')),
-    # path('faculty/', include('api.faculty.urls')),
-    # path('info/', include('api.info.urls')),
-    # path('role/', include('api.role.urls')),
+    path('comment/', include('api.comment.urls')),
+    path('contribution/', include('api.contribution.urls')),
+    path('faculty/', include('api.faculty.urls')),
+    path('info/', include('api.info.urls')),
+    path('role/', include('api.role.urls')),
     path('user/', include('api.user.urls')),
 ]
