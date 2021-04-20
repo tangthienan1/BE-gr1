cf07c2e3baaf4de08df936582f033793082398bd
Nguyen Do <neyugn.dh@gmail.com>
2021-04-20 15:40:30 +0700

    fix

--- BE/api/contribution/views.py
+++ BE/api/contribution/views.py
@@ -13,24 +13,24 @@ class ContributionViewSet(viewsets.ModelViewSet):
     serializer_class = ContributionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    '''
+
     def create(self, request, *args, **kwargs):
         serializer = ContributionCreateSerializer(data=request.data)
-        current_info = self.request.user.info
         serializer.is_valid(raise_exception=True)
+        current_info = self.request.user.info
         serializer.save(author=current_info, faculty=current_info.faculty)
-        recipient = Info.objects.filter(faculty_id=serializer.validated_data.get('faculty_id')).filter(role_id=2)
+        recipient = Info.objects.get(faculty_id=current_info.faculty_id, role_id=2)
         recipient_email = recipient.email
         send_mail(
             'New Contribution Notification',
             'A new contribution has been submitted within your faculty.\nPlease review within 14 days.',
             'no-reply@example.com',
-            recipient_email, # coordinator emails here
+            [recipient_email], # coordinator emails here
             fail_silently=False,
         )
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    '''
+
     '''
     def perform_create(self, serializer):
         current_info = self.request.user.info
@@ -47,22 +47,13 @@ class ContributionViewSet(viewsets.ModelViewSet):
     '''
 
     # Get list of contributions by faculty_id. Example URL : api/contribution/by-faculty/1/
-    @action(detail=False, methods=['get'], url_path=r'^by-contribution/(?P<faculty_id>\d+)/$') #(?P<faculty_id>\d+)/$
+    @action(detail=False, methods=['get'], url_path=r'^by-faculty/(?P<faculty_id>\d+)/$')
     def faculty(self, request, *args, **kwargs):
         queryset = Contribution.objects.filter(faculty=self.kwargs['faculty_id'])
         serializer = ContributionSerializer(queryset, many=True)
         if not queryset:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.data)
-
-
-    # def retrieve(self, pk=None):
-    #     contributions = Contribution.objects.filter(faculty_id=pk)
-    
-    # def get_queryset(self):
-    #     queryset = self.queryset
-    #     contributions = queryset.filter(faculty=self.kwargs['faculty_id'])
-    #     return contributions
 
 
 
